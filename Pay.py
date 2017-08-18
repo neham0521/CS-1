868ccefe30b0b816de84aae05ec2275eed388224
neham0521 <30518613+neham0521@users.noreply.github.com>
2017-08-18 22:44:31 +1000

    Add files via upload

--- Pay.py
+++ Pay.py
@@ -1,3 +1,14 @@
-hours = input('hours')
-rate=input('rate')
-print('Pay=',int(hours)*int(rate))
+try:
+	hours = int(input('hours'))
+	rate=int(input('rate'))
+	if hours <= 40:
+		print('Pay=',hours*rate)
+	if hours > 40:
+		extrahours = hours - 40
+		print('extrahours =',extrahours)
+		extrapay = extrahours*rate*1.5
+		pay = ((hours-extrahours)*rate) + extrapay
+		print('Pay=',pay)
+except:
+	print('please enter a numeric input')
+#Ques: I want to again prompt the user for input - not sure how.
